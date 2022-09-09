cb37b90f377f127a6d8b7bfb1753cc19b45e6af3
pho <phonum5@gmail.com>
2022-09-08 17:15:51 -0700

    minor fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	return (0);
 }
