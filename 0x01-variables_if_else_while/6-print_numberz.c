1d52c5c528471ed83137e60379193daa75e00548
pho <phonum5@gmail.com>
2022-09-09 22:28:09 +0000

    print numbers but with putchar() only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
