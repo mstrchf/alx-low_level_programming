4d19951dac57b46a4dcd2e1f8cf62557a79472cd
pho <phonum5@gmail.com>
2022-09-09 22:14:57 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
