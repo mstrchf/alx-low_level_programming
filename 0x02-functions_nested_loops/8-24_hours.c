e8af01cc1f572da6d8c2df7fc356fe413664732e
pho <phonum5@gmail.com>
2022-09-13 21:07:05 +0000

    minor fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,19 +11,25 @@ void jack_bauer(void)
 
 	for(h1 = 0; h1 <= 2; h1++)
 	{
-		for (h2 = 0; h2 <= 3; h2++)
+		for (h2 = 0; h2 <= 9; h2++)
 		{
 			int m1, m2;
 			for (m1 = 0; m1 <= 5; m1++)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
 				{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m1 + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
+					if (h1 > 2 && h2 > 3)
+					{
+					}
+					else
+					{
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
+						_putchar(':');
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
