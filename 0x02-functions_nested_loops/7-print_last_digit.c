077f6205287cc08c86320c57b0bb5e7dd6f31f4e
pho <phonum5@gmail.com>
2022-09-13 17:02:51 +0000

    print last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of number n
+ * @n: number to get last digit from
+ *
+ * Return: integer, last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n & 10);
+}
