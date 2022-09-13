77aa656569c77662174dc2f5e481ec4e59920079
pho <phonum5@gmail.com>
2022-09-13 22:30:31 +0000

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,36 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		if (n == 98)
+		while (n <= 98)
 		{
-			printf("%i\n", n);
-		}
-		else
-		{
-			printf("%i, ", n);
-		}
+			if (n == 98)
+			{
+				printf("%i\n", n);
+			}
+			else
+			{
+				printf("%i, ", n);
+			}
 
-		n++;
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			if (n == 98)
+			{
+				printf("%i\n", n);
+			}
+			else
+			{
+				printf("%i, ", n);
+			}
+
+			n--;
+		}
 	}
 }
