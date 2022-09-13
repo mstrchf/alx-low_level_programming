f86d684788b1c2a87f978004d7c8f5aa8f69daf4
pho <phonum5@gmail.com>
2022-09-13 21:11:56 +0000

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,12 @@ void jack_bauer(void)
 {
 	int h1, h2;
 
-	for(h1 = 0; h1 <= 2; h1++)
+	for (h1 = 0; h1 <= 2; h1++)
 	{
 		for (h2 = 0; h2 <= 9; h2++)
 		{
 			int m1, m2;
+
 			for (m1 = 0; m1 <= 5; m1++)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
