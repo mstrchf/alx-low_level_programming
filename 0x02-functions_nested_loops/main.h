1c96bec38055d87c103e60c7e42dbb8838a651d5
pho <phonum5@gmail.com>
2022-09-13 13:31:44 +0000

    add main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,6 @@
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+/* function prototypes */
+int _putchar(char c);
