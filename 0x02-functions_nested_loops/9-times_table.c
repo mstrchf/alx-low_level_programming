2690b716f4adb17cf01c6cfba9f2def22ef8af6b
pho <phonum5@gmail.com>
2022-09-13 21:38:34 +0000

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, col;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			int result;
+			
+			result = row * col;
+
+			if (result >= 10)
+			{
+				int last_digit, first_digit;
+
+				last_digit = result % 10;
+				first_digit = result - last_digit;
+				first_digit = first_digit / 10;
+
+				_putchar(first_digit + '0');
+				_putchar(last_digit + '0');
+				
+				if (col < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(result + '0');
+				
+				if (col < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
