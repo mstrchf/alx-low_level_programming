1d2c359ecfbfa06d59167b27bae29d1ba1678c7b
pho <phonum5@gmail.com>
2022-09-13 15:45:22 +0000

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	for (count = 0; count < 10; count++)
+	{
+		char ch;
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		
+		_putchar('\n');
+	}
+}
