04547850aca8f6f0c6c9c3f9c8b63b49f0016f56
pho <phonum5@gmail.com>
2022-09-13 16:55:12 +0000

    return absolute value of number n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of number
+ * @n: input
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
