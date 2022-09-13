827344479f997f28169c309445ec0af7fcffe4f1
pho <phonum5@gmail.com>
2022-09-13 22:00:22 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		for (col = 0; col <= 9; col++)
 		{
 			int result;
-			
+
 			result = row * col;
 
 			if (result >= 10)
@@ -23,11 +23,8 @@ void times_table(void)
 				last_digit = result % 10;
 				first_digit = result - last_digit;
 				first_digit = first_digit / 10;
-
-				
 				_putchar(',');
 				_putchar(' ');
-
 				_putchar(first_digit + '0');
 				_putchar(last_digit + '0');
 			}
