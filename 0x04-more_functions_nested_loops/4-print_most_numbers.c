46dbbffab51257df3645b37d10ab3d3b35757870
pho <phonum5@gmail.com>
2022-09-15 12:14:40 +0000

    print numbers from 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0 to 9 excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 || num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+
+	_putchar('\n');
+}
