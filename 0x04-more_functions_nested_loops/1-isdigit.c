353b564b70643c1a896daf2482bcf191f08e7b93
pho <phonum5@gmail.com>
2022-09-15 10:43:31 +0000

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if char c is digit
+ * @c: character to check
+ *
+ * Return: 1 if char is digit
+ * else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
