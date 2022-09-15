a1888956d803c57817eb5a3ca6e83d2d84e91abe
pho <phonum5@gmail.com>
2022-09-15 21:21:04 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ * @n: number of lines
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = n - i; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
