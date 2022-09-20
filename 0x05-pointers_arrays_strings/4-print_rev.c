912b85349b09ce80b6877e489994dd9d3b73c553
pho <phonum5@gmail.com>
2022-09-20 00:52:31 +0000

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,3 +17,26 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - return the length of string s
+ * @s: string
+ *
+ * Return: int, length of string s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int counter;
+
+	counter = 0;
+	for (i = 0; ; i++)
+	{
+		if (*(s + i) == '\0')
+		{
+			return (counter);
+		}
+		counter++;
+	}
+}
