e67dd9f32bb5ea90f600cbbcfb1a395942920adf
pho <phonum5@gmail.com>
2022-09-22 23:14:17 +0000

    capitalize first letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words to upper
+ * @s: string
+ *
+ * Return: string pointer
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		switch (s[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+				break;
+			default:
+				break;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
