1da840a6a399612086152579ad2c1c44ab9b2580
pho <phonum5@gmail.com>
2022-09-22 21:18:10 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array items
+ * @a: array pointer
+ * @n: number of elements in array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int temp[n];
+
+	i = 0
+	while (n > 0)
+	{
+		temp[i] = a[n - 1];
+		n--;
+		i++;
+	}
+
+	j = 0;
+	while (j < i)
+	{
+		a[j] = temp[j];
+	}
+}
