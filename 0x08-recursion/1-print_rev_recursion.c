3b5ab49aec2a8e58f952ebb99ef678b24724d078
pho <phonum5@gmail.com>
2022-09-28 19:22:12 +0000

    print string recursively in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - recursively print string s in reverse
+ * @s: string pointer
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
