52a567601b75dba71f392db2f816e155e37ad870
pho <phonum5@gmail.com>
2022-10-03 20:37:36 +0000

    minor fix

--- 0x08-recursion/test.c
+++ 0x08-recursion/test.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
+	printf("%d\n", _pow_recursion(1, 10));
+	printf("%d\n", _pow_recursion(1024, 0));
+	printf("%d\n", _pow_recursion(2, 16));
+	printf("%d\n", _pow_recursion(5, 2));
+	printf("%d\n", _pow_recursion(5, -2));
+	printf("%d\n", _pow_recursion(-5, 3));
 	return (0);
 }
